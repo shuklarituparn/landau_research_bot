17eaea068efcfef78afa35324c9df088e78f0143
Rituparn Shukla <shukla.r@phystech.edu>
2024-04-26 11:34:14 +0300

    added code

--- bot/ai_helper.py
+++ bot/ai_helper.py
@@ -4,27 +4,30 @@ from langchain.chat_models.gigachat import GigaChat
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ContextTypes
+from langchain_community.retrievers import TavilySearchAPIRetriever
 
 load_dotenv()
 """Загружаем Ключи Доступа"""
-TOKEN = os.getenv("GIGA_CHAT_TOKEN")
+TOKEN = os.getenv("GIGACHAT_API_AUTH")
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY=")
 
 
 async def ai_help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Функция, которая вызывает помощник по искусственному интеллекту с помощью ключа доступа"""
     chat = GigaChat(credentials=TOKEN, verify_ssl_certs=False)
+    #
+    # messages = [
+    #     SystemMessage(
+    #         content=""
+    #     )
+    # ]
 
-    messages = [
-        SystemMessage(
-            content="Ты мужской эмпатичный бот-психолог-помощник по именем Аади который помогает пользователю решить его проблемы."
-        )
-    ]
+    retriever = TavilySearchAPIRetriever(k=3)
+
+    result = retriever.invoke(update.message.text)  #getting the search result
 
     """Получение текста от пользователя и отправка ответа от помощника пользователю"""
-    user_input = update.message.text
-    messages.append(HumanMessage(content=user_input))
-    res = chat(messages)
-    messages.append(res)
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id, text=f"Помощник:\n{res.content}"
-    )
+    # user_input = update.message.text
+    # messages.append(HumanMessage(content=user_input))
+    # res = chat(messages)
+    # messages.append(res)
+    return result
---
+++ bot/utils/translate_utils/translate.py
@@ -0,0 +1,20 @@
+import os
+
+from langchain_community.chat_models import GigaChat
+from langchain_community.document_transformers import DoctranTextTranslator
+from langchain_core.documents import Document
+from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage
+
+load_dotenv()
+GIGACHAT_CREDENTIALS = os.getenv("GIGACHAT_API_AUTH")
+
+async def translate_text(content):
+    chat = GigaChat(credentials=GIGACHAT_CREDENTIALS, verify_ssl_certs=False)
+
+    messages = [SystemMessage(content="")]
+
+    messages.append(HumanMessage(content=content))
+    # res = chat(messages)
+    # messages.append(res)
+    return result
